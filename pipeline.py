4fa23be70c29874670ea84d61a208b7ec1597cec
QuadraticAcid <75771953+blackrabbit17@users.noreply.github.com>
2022-09-30 16:37:58 +1300

    Update pipeline.py with next steps

--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,17 @@ import os
 from random import random, randint
 from mlflow import log_metric, log_param, log_artifacts
 
+# First Step: 
+# https://mlflow.org/docs/latest/tutorials-and-examples/tutorial.html
+# Do this tutorial : You can copy and paste the code, but the point is you need to understand what
+# every single line of code is doing.
+# 
+# Use the debugger, or print() statements, so you can watch exectution flow as it happens
+# practice explaining it to youself and then when you're done, let's have a google-meeting
+# and I will ask you heaps of questions about the code
+# 
 
+# Then after that, we can come up with a set of steps to do the QM7 Dataset!
 
 if __name__ == "__main__":
     # Log a parameter (key-value pair)

9f6417f6a04941f96d5c7f178cf327f63128051b
Aprameya <82435060+aprameyap@users.noreply.github.com>
2022-10-05 08:22:49 +0530

    Delete qm7.py

--- qm7.py
+++
@@ -1,9 +0,0 @@
-import deepchem as dc
-from deepchem.molnet.load_function.qm7_datasets import load_qm7
-
- #loading qm7
-
-(tasks, datasets, transformers) = dc.molnet.load_qm7(move_mean=False)
-(train_dataset, valid_dataset, test_dataset) = datasets
-
-regression_metric = dc.metrics.Metric(dc.metrics.mean_absolute_error, mode="regression")
